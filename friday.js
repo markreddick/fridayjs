967d6652e986e1b81faefffcbf8fa1c399723573
Mark Reddick <mark@mersjr.com>
2020-10-20 18:02:16 +0000

    reverted back to using prototypes

--- friday.js
+++ friday.js
@@ -2,75 +2,92 @@ define(function() {
 
 	'use strict';
 
-	/*
-	class ZippyElements  {
-		constructor(elements) {
-			this.elements = elements;
-		}
+	var FridayElements = function(elements) {
+		this.elements = elements;
 	};
 
-	var zippy_elements = function(elements) {
-		this.elements = elements;
-	}
+	var friday = function(selector) {
 
-	zippy_elements.prototype.addClass = function (class_name) {
+		var elements = null;
+
+		console.log('friday init called');
+
+		if (selector === 'document') {
+			elements = [document];
+		} else if (selector === 'window') {
+			elements = [window];
+		} else {
+			elements = document.querySelectorAll(selector);
+		}
+
+		return new FridayElements(elements);
+	};
+
+	FridayElements.prototype.addClass = function(class_name) {
+		var i, j;
 		var class_names = class_name.split(' ');
-		for (var i = 0; i < this.elements.length; i++) {
-			for (var j = 0; j < class_names.length; j++) {
+		for (i = 0; i < this.elements.length; i++) {
+			for (j = 0; j < class_names.length; j++) {
 				this.elements[i].classList.add(class_names[j]);
 			}
 		}
 		return this;
 	};
 
-	zippy_elements.prototype.attr = function(name, value) {
-		for (var i = 0; i < this.elements.length; i++) {
+	FridayElements.prototype.attr = function(name, value) {
+		var i;
+		for (i = 0; i < this.elements.length; i++) {
 			this.elements[i].setAttribute(name, value);
 		}
 		return this;
-	}
+	};
 
-	zippy_elements.prototype.each = function(callback) {
+	FridayElements.prototype.each = function(callback) {
+		var i;
 		if (!callback || typeof callback !== 'function') return;
-		for (var i = 0; i < this.elements.length; i++) {
+		for (i = 0; i < this.elements.length; i++) {
 			callback(this.elements[i], i);
 		}
 		return this;
 	};
 
-	zippy_elements.prototype.on = function(events, handler) {
+	FridayElements.prototype.on = function(events, handler) {
+		var i, j;
 		var event_list = events.split(' ');
-		for (var i = 0; i < this.elements.length; i++) {
-			for (var j = 0; j < event_list.length; j++) {
+		for (i = 0; i < this.elements.length; i++) {
+			for (j = 0; j < event_list.length; j++) {
 				this.elements[i].addEventListener(event_list[j], handler);
 			}
 		}
 		return this;
 	};
 
-	zippy_elements.prototype.removeAttr = function(names) {
+	FridayElements.prototype.removeAttr = function(names) {
+		var i, j;
 		var name_list = names.split(' ');
-		for (var i = 0; i < this.elements.length; i++) {
-			for (var j = 0; j < name_list.length; j++) {
+		for (i = 0; i < this.elements.length; i++) {
+			for (j = 0; j < name_list.length; j++) {
 				this.elements[i].removeAttribute(name_list[j]);
 			}
 		}
 		return this;
-	}
+	};
 
-	zippy_elements.prototype.removeClass = function(class_name) {
+	FridayElements.prototype.removeClass = function(class_name) {
+		var i, j;
 		var class_names = class_name.split(' ');
-		for (var i = 0; i < this.elements.length; i++) {
-			for (var j = 0; j < class_names.length; j++) {
+		for (i = 0; i < this.elements.length; i++) {
+			for (j = 0; j < class_names.length; j++) {
 				this.elements[i].classList.remove(class_names[j]);
 			}
-		};
+		}
 		return this;
 	};
 
-	zippy_elements.prototype.text = function(new_text) {
+	FridayElements.prototype.text = function(new_text) {
+		var i;
 		var out_text = '';
-		for (var i = 0; i < this.elements.length; i++) {
+		for (i = 0; i < this.elements.length; i++) {
 			if (new_text) {
 				this.elements[i].textContent = new_text;
 			} else {
@@ -82,11 +99,12 @@ define(function() {
 		} else {
 			return out_text;
 		}
-	}
+	};
 
-	zippy_elements.prototype.val = function (new_value) {
+	FridayElements.prototype.val = function(new_value) {
+		var i;
 		var vals = [];
-		for (var i = 0; i < this.elements.length; i++) {
+		for (i = 0; i < this.elements.length; i++) {
 			if (new_value) {
 				this.elements[i].value = new_value;
 			} else {
@@ -103,24 +121,9 @@ define(function() {
 				return vals;
 			}
 		}
-	}
-
-	var zippy = function(selector) {
-
-		var elements = null;
-
-		if (selector === 'document') {
-			elements = [document];
-		} else if (selector === 'window') {
-			elements = [window];
-		} else {
-			elements = document.querySelectorAll(selector);
-		}
-
-		return new zippy_elements(elements);
 	};
 
-	zippy.post = function(url, data) {
+	friday.post = function(url, data) {
 		var f = fetch(url, {
 			'method': 'POST',
 			'mode': 'cors',
@@ -132,41 +135,9 @@ define(function() {
 			'redirect': 'follow',
 			'referrer': 'no-referrer',
 			'body': JSON.stringify(data),
-		})
-		.then(response => response.json());
+		}).then((response) => response.json());
 
 		return f;
-	};
-
-	return zippy;
-	*/
-
-	var friday = function(selector) {
-
-		var elements = null;
-		var i, j;
-
-		var addClass = function(class_name) {
-
-			var class_names = class_name.split(' ');
-			for (i = 0; i < elements.length; i++) {
-				for (j = 0; j < class_names.length; j++) {
-					elements[i].classList.add(class_names[j]);
-				}
-			}
-		};
-
-		if (selector === 'document') {
-			elements = [document];
-		} else if (selector === 'window') {
-			elements = [window];
-		} else {
-			elements = document.querySelectorAll(selector);
-		}
-
-		return {
-			addClass: addClass,
-		};
 	};
 
 	return friday;
